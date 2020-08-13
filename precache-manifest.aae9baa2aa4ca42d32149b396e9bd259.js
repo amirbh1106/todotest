355b49e9c3fccda28a538b899ec68eb8072e00c7
amirbh1106 <amirbh110@outlook.com>
2020-08-13 21:33:26 +0300

    backend

---
+++ precache-manifest.aae9baa2aa4ca42d32149b396e9bd259.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6452e2abaa9164b4fbb629962e438f",
+    "url": "/todotest/index.html"
+  },
+  {
+    "revision": "5c486855ad2d2acd17ff",
+    "url": "/todotest/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c7e6c8c899b2508102fa",
+    "url": "/todotest/static/css/main.559d6943.chunk.css"
+  },
+  {
+    "revision": "5c486855ad2d2acd17ff",
+    "url": "/todotest/static/js/2.a3136b09.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todotest/static/js/2.a3136b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e6c8c899b2508102fa",
+    "url": "/todotest/static/js/main.3bb4e5ec.chunk.js"
+  },
+  {
+    "revision": "2062479fda0d74fb6e05",
+    "url": "/todotest/static/js/runtime-main.eb50593f.js"
+  }
+]);
