674a6bd2d8151073fee105e4633e09ec97a89019
amirbh1106 <amirbh110@outlook.com>
2020-08-13 21:33:26 +0300

    backend

---
+++ serverside/index.ts
@@ -0,0 +1,17 @@
+const express = require( "express" );
+const app = express();
+const testapikey = "absdljskdajldksjald"
+
+app.get("/test/apikey=:apiKey/", (req, res) => {
+    console.log(req.params.apiKey);
+    if(req.params.apiKey == testapikey){
+        res.send("aprove")
+    }else{res.send("denied")}
+   
+})
+
+app.listen(5000);
+console.log("server is on on port 5000")
+
+
+
